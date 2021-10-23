164bd82070e872a4ce627cc7252a22e4b33c007c
Jialu <dylanshen98@gmail.com>
2021-10-23 22:55:57 +0800

    Create TourInfo.vue
    HTML and CSS converted

---
+++ src/views/TourInfo.vue
@@ -0,0 +1,801 @@
+<template>
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <link
+        href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap"
+        rel="stylesheet"
+      />
+      <link href="./css/main.css" rel="stylesheet" />
+      <title>Document</title>
+      <link href="./main.css" rel="stylesheet" />
+      <title>Document</title>
+      <link href="./main.css" rel="stylesheet" />
+      <title>Document</title>
+    </head>
+
+    <body>
+      <div class="v97_34">
+        <div class="v261_56">
+          <div class="name"></div>
+          <span class="v97_485">Describe your experience</span>
+          <div class="name"></div>
+          <span class="v264_76"
+            >When will you be free to host the experience?</span
+          ><span class="v264_80"
+            >Please select your mode of transportation provided for your
+            guests:</span
+          ><span class="v264_84"
+            >How many years of tour experience do you have?</span
+          ><span class="v264_89">How much would the experience cost?</span
+          ><span class="v264_98">Add photos to describe your tour</span
+          ><span class="v264_124">Name your tour</span>
+          <div class="v97_504"></div>
+          <div class="v264_87"></div>
+          <div class="v264_90"></div>
+          <div class="v264_125"></div>
+          <div class="v264_61"></div>
+          <div class="v264_99"></div>
+          <div class="v97_507">
+            <div class="v97_508"><div class="v97_509"></div></div>
+            <div class="v97_511">
+              <div class="v97_512"></div>
+              <div class="v97_513"></div>
+            </div>
+          </div>
+          <span class="v97_514">23 Sep 2021 - 30 Dec 2021</span
+          ><span class="v97_516"> </span>
+          <div class="v97_517"><div class="v97_518"></div></div>
+          <div class="v97_519"><div class="v97_520"></div></div>
+          <div class="name"></div>
+          <div class="v151_59"><span class="v151_61">First Name:</span></div>
+          <div class="v151_203"></div>
+        </div>
+        <span class="v264_56"
+          >What will you and your guests do? Provide a detailed timeline of the
+          tour from start to finish Describe how your tour stands out from the
+          rest!
+        </span>
+        <div class="v264_123">
+          <span class="v264_121">Tour Name</span
+          ><span class="v264_63">What you’ll do</span
+          ><span class="v264_96">Add photos</span>
+          <div class="name"></div>
+          <span class="v151_143">Duration Offered</span
+          ><span class="v264_85">Work Experience</span>
+          <div class="v264_78"></div>
+          <div class="v264_64"></div>
+          <div class="v264_86"></div>
+          <div class="v264_79"></div>
+          <div class="v264_122"></div>
+          <div class="v264_92"></div>
+          <div class="v264_97"></div>
+          <div class="v264_62"></div>
+          <span class="v264_77">Transportation</span
+          ><span class="v264_93">Cost</span>
+        </div>
+        <span class="v264_65"
+          >Describe your tour to stand out from the rest! A good description
+          usually has more than 200 words. </span
+        ><span class="v264_81">Public Transport</span>
+        <div class="v264_82"><div class="v264_83"></div></div>
+        <span class="v264_88">Input the number of years in numbers.</span
+        ><span class="v264_91">Price of tour.</span
+        ><span class="v264_100">Add photos</span>
+        <div class="v264_126"></div>
+        <span class="v264_128">List</span>
+      </div>
+    </body>
+  </html>
+</template>
+
+<script>
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  setup() {
+    TODO;
+  },
+});
+</script>
+
+
+
+
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+body {
+  font-size: 14px;
+}
+.v97_34 {
+  width: 100%;
+  height: 2096px;
+  background: rgba(255, 251, 251, 1);
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v261_56 {
+  width: 100%;
+  height: 2338px;
+  background: url("../images/v261_56.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 441px;
+  left: 43px;
+  overflow: hidden;
+}
+.name {
+  color: #fff;
+}
+.v97_485 {
+  width: 724px;
+  color: rgba(0, 0, 0, 1);
+  position: absolute;
+  top: 511px;
+  left: 474px;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: Ubuntu;
+  font-weight: Bold;
+  font-size: 50px;
+  opacity: 1;
+  text-align: left;
+}
+.name {
+  color: #fff;
+}
+.v264_76 {
+  width: 661px;
+  color: rgba(0, 0, 0, 1);
+  position: absolute;
+  top: 1092px;
+  left: 403px;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: Ubuntu;
+  font-weight: Bold;
+  font-size: 25px;
+  opacity: 1;
+  text-align: left;
+}
+.v264_80 {
+  width: 902px;
+  color: rgba(0, 0, 0, 1);
+  position: absolute;
+  top: 1253px;
+  left: 403px;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: Ubuntu;
+  font-weight: Bold;
+  font-size: 25px;
+  opacity: 1;
+  text-align: left;
+}
+.v264_84 {
+  width: 902px;
+  color: rgba(0, 0, 0, 1);
+  position: absolute;
+  top: 1420px;
+  left: 403px;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: Ubuntu;
+  font-weight: Bold;
+  font-size: 25px;
+  opacity: 1;
+  text-align: left;
+}
+.v264_89 {
+  width: 902px;
+  color: rgba(0, 0, 0, 1);
+  position: absolute;
+  top: 1578px;
+  left: 403px;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: Ubuntu;
+  font-weight: Bold;
+  font-size: 25px;
+  opacity: 1;
+  text-align: left;
+}
+.v264_98 {
+  width: 902px;
+  color: rgba(0, 0, 0, 1);
+  position: absolute;
+  top: 1726px;
+  left: 403px;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: Ubuntu;
+  font-weight: Bold;
+  font-size: 25px;
+  opacity: 1;
+  text-align: left;
+}
+.v264_124 {
+  width: 902px;
+  color: rgba(0, 0, 0, 1);
+  position: absolute;
+  top: 2238px;
+  left: 398px;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: Ubuntu;
+  font-weight: Bold;
+  font-size: 25px;
+  opacity: 1;
+  text-align: left;
+}
+.v97_504 {
+  width: 920px;
+  height: 58px;
+  background: rgba(255, 255, 255, 1);
+  opacity: 1;
+  position: absolute;
+  top: 1302px;
+  left: 403px;
+  border: 1px solid rgba(0, 0, 0, 0.33000001311302185);
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  overflow: hidden;
+}
+.v264_87 {
+  width: 920px;
+  height: 58px;
+  background: rgba(255, 255, 255, 1);
+  opacity: 1;
+  position: absolute;
+  top: 1471px;
+  left: 403px;
+  border: 1px solid rgba(0, 0, 0, 0.33000001311302185);
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  overflow: hidden;
+}
+.v264_90 {
+  width: 920px;
+  height: 58px;
+  background: rgba(255, 255, 255, 1);
+  opacity: 1;
+  position: absolute;
+  top: 1629px;
+  left: 403px;
+  border: 1px solid rgba(0, 0, 0, 0.33000001311302185);
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  overflow: hidden;
+}
+.v264_125 {
+  width: 920px;
+  height: 58px;
+  background: rgba(255, 255, 255, 1);
+  opacity: 1;
+  position: absolute;
+  top: 2280px;
+  left: 394px;
+  border: 1px solid rgba(0, 0, 0, 0.33000001311302185);
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  overflow: hidden;
+}
+.v264_61 {
+  width: 920px;
+  height: 221px;
+  background: rgba(255, 255, 255, 1);
+  opacity: 1;
+  position: absolute;
+  top: 822px;
+  left: 403px;
+  border: 2px solid rgba(0, 0, 0, 0.33000001311302185);
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  overflow: hidden;
+}
+.v264_99 {
+  width: 920px;
+  height: 413px;
+  background: rgba(255, 255, 255, 1);
+  opacity: 1;
+  position: absolute;
+  top: 1776px;
+  left: 403px;
+  border: 1px solid rgba(0, 0, 0, 1);
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+  overflow: hidden;
+}
+.v97_507 {
+  width: 920px;
+  height: 58px;
+  background: url("../images/v97_507.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 1134px;
+  left: 403px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  overflow: hidden;
+}
+.v97_508 {
+  width: 920px;
+  height: 58px;
+  background: url("../images/v97_508.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  overflow: hidden;
+}
+.v97_509 {
+  width: 920px;
+  height: 58px;
+  background: rgba(255, 255, 255, 1);
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  border: 1px solid rgba(0, 0, 0, 0.33000001311302185);
+}
+.v97_511 {
+  width: 48px;
+  height: 42px;
+  background: rgba(255, 255, 255, 1);
+  opacity: 1;
+  position: absolute;
+  top: 10px;
+  left: 849px;
+  overflow: hidden;
+}
+.v97_512 {
+  width: 39px;
+  height: 38px;
+  background: rgba(63, 163, 184, 1);
+  opacity: 1;
+  position: absolute;
+  top: 1px;
+  left: 4px;
+}
+.v97_513 {
+  width: 48px;
+  height: 42px;
+  background: url("../images/v97_513.png");
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+}
+.v97_514 {
+  width: 561px;
+  color: rgba(0, 0, 0, 1);
+  position: absolute;
+  top: 1155px;
+  left: 437px;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: Ubuntu;
+  font-weight: Regular;
+  font-size: 25px;
+  opacity: 1;
+  text-align: left;
+}
+.v97_516 {
+  color: rgba(0, 0, 0, 1);
+  position: absolute;
+  top: 1458px;
+  left: 1006px;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: Ubuntu;
+  font-weight: Regular;
+  font-size: 25px;
+  opacity: 1;
+  text-align: left;
+}
+.v97_517 {
+  width: 35px;
+  height: 21px;
+  background: rgba(255, 255, 255, 1);
+  opacity: 1;
+  position: absolute;
+  top: 1171px;
+  left: 567px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  overflow: hidden;
+}
+.v97_518 {
+  width: 35px;
+  height: 21px;
+  background: url("../images/v97_518.png");
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+}
+.v97_519 {
+  width: 35px;
+  height: 21px;
+  background: rgba(255, 255, 255, 1);
+  opacity: 1;
+  position: absolute;
+  top: 1171px;
+  left: 603px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  overflow: hidden;
+}
+.v97_520 {
+  width: 35px;
+  height: 21px;
+  background: url("../images/v97_520.png");
+  opacity: 1;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+}
+.name {
+  color: #fff;
+}
+.v151_59 {
+  width: 179px;
+  height: 30px;
+  background: url("../images/v151_59.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 61px;
+  left: 718px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  overflow: hidden;
+}
+.v151_61 {
+  width: 179px;
+  color: url("../images/v151_61.png");
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  font-family: Ubuntu;
+  font-weight: Regular;
+  font-size: 25px;
+  opacity: 1;
+  text-align: left;
+}
+.v151_203 {
+  width: 114px;
+  height: 112px;
+  background: url("../images/v151_203.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 478px;
+  left: 112px;
+  border-top-left-radius: 100px;
+  border-top-right-radius: 100px;
+  border-bottom-left-radius: 100px;
+  border-bottom-right-radius: 100px;
+  overflow: hidden;
+}
+.v264_56 {
+  width: 696px;
+  color: rgba(0, 0, 0, 1);
+  position: absolute;
+  top: 235px;
+  left: 361px;
+  font-size: 25px;
+  opacity: 1;
+  text-align: left;
+}
+.v264_123 {
+  width: 413px;
+  height: 637px;
+  background: url("../images/v264_123.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  position: absolute;
+  top: 235px;
+  left: 33px;
+  overflow: hidden;
+}
+.v264_121 {
+  width: 192px;
+  color: rgba(0, 0, 0, 1);
+  position: absolute;
+  top: 600px;
+  left: 44px;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: Ubuntu;
+  font-weight: Regular;
+  font-size: 25px;
+  opacity: 1;
+  text-align: left;
+}
+.v264_63 {
+  width: 183px;
+  color: rgba(0, 0, 0, 1);
+  position: absolute;
+  top: 0px;
+  left: 44px;
+  font-family: Ubuntu;
+  font-weight: Regular;
+  font-size: 25px;
+  opacity: 1;
+  text-align: left;
+}
+.v264_96 {
+  width: 192px;
+  color: rgba(0, 0, 0, 1);
+  position: absolute;
+  top: 507px;
+  left: 40px;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: Ubuntu;
+  font-weight: Regular;
+  font-size: 25px;
+  opacity: 1;
+  text-align: left;
+}
+.name {
+  color: #fff;
+}
+.v151_143 {
+  width: 370px;
+  color: rgba(0, 0, 0, 1);
+  position: absolute;
+  top: 118px;
+  left: 42px;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: Ubuntu;
+  font-weight: Regular;
+  font-size: 25px;
+  opacity: 1;
+  text-align: left;
+}
+.v264_85 {
+  width: 192px;
+  color: rgba(0, 0, 0, 1);
+  position: absolute;
+  top: 309px;
+  left: 44px;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: Ubuntu;
+  font-weight: Regular;
+  font-size: 25px;
+  opacity: 1;
+  text-align: left;
+}
+.v264_78 {
+  width: 28px;
+  height: 24px;
+  background: rgba(255, 255, 255, 1);
+  opacity: 1;
+  position: absolute;
+  top: 112px;
+  left: 1px;
+  border-radius: 50%;
+}
+.v264_64 {
+  width: 28px;
+  height: 24px;
+  background: rgba(255, 255, 255, 1);
+  opacity: 1;
+  position: absolute;
+  top: 112px;
+  left: 1px;
+  border-radius: 50%;
+}
+.v264_86 {
+  width: 28px;
+  height: 24px;
+  background: rgba(255, 255, 255, 1);
+  opacity: 1;
+  position: absolute;
+  top: 306px;
+  left: 0px;
+  border-radius: 50%;
+}
+.v264_79 {
+  width: 28px;
+  height: 24px;
+  background: rgba(255, 255, 255, 1);
+  opacity: 1;
+  position: absolute;
+  top: 213px;
+  left: 1px;
+  border-radius: 50%;
+}
+.v264_122 {
+  width: 28px;
+  height: 24px;
+  background: rgba(255, 255, 255, 1);
+  opacity: 1;
+  position: absolute;
+  top: 612px;
+  left: 0px;
+  border-radius: 50%;
+}
+.v264_92 {
+  width: 28px;
+  height: 24px;
+  background: rgba(255, 255, 255, 1);
+  opacity: 1;
+  position: absolute;
+  top: 407px;
+  left: 0px;
+  border-radius: 50%;
+}
+.v264_97 {
+  width: 28px;
+  height: 24px;
+  background: rgba(255, 255, 255, 1);
+  opacity: 1;
+  position: absolute;
+  top: 516px;
+  left: 0px;
+  border-radius: 50%;
+}
+.v264_62 {
+  width: 28px;
+  height: 24px;
+  background: rgba(63, 163, 184, 1);
+  opacity: 1;
+  position: absolute;
+  top: 22px;
+  left: 1px;
+  border-radius: 50%;
+}
+.v264_77 {
+  width: 170px;
+  color: rgba(0, 0, 0, 1);
+  position: absolute;
+  top: 214px;
+  left: 42px;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: Ubuntu;
+  font-weight: Regular;
+  font-size: 25px;
+  opacity: 1;
+  text-align: left;
+}
+.v264_93 {
+  width: 192px;
+  color: rgba(0, 0, 0, 1);
+  position: absolute;
+  top: 398px;
+  left: 44px;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: Ubuntu;
+  font-weight: Regular;
+  font-size: 25px;
+  opacity: 1;
+  text-align: left;
+}
+.v264_65 {
+  width: 901px;
+  color: rgba(41, 37, 37, 0.33000001311302185);
+  position: absolute;
+  top: 370px;
+  left: 370px;
+  font-family: Ubuntu;
+  font-weight: Italic;
+  font-size: 25px;
+  opacity: 1;
+  text-align: left;
+}
+.v264_81 {
+  width: 460px;
+  color: rgba(0, 0, 0, 1);
+  position: absolute;
+  top: 872px;
+  left: 380px;
+  font-family: Ubuntu;
+  font-weight: Regular;
+  font-size: 25px;
+  opacity: 1;
+  text-align: left;
+}
+.v264_82 {
+  width: 55px;
+  height: 50px;
+  background: rgba(255, 255, 255, 1);
+  opacity: 0.699999988079071;
+  position: absolute;
+  top: 920px;
+  left: 1214px;
+  transform: rotate(-89deg);
+  overflow: hidden;
+}
+.v264_83 {
+  width: 16px;
+  height: 29px;
+  background: rgba(0, 0, 0, 1);
+  opacity: 1;
+  position: absolute;
+  top: 19px;
+  left: 10px;
+}
+.v264_88 {
+  width: 549px;
+  color: rgba(41, 37, 37, 0.33000001311302185);
+  position: absolute;
+  top: 1035px;
+  left: 380px;
+  font-family: Ubuntu;
+  font-weight: Italic;
+  font-size: 25px;
+  opacity: 1;
+  text-align: left;
+}
+.v264_91 {
+  width: 549px;
+  color: rgba(41, 37, 37, 0.33000001311302185);
+  position: absolute;
+  top: 1193px;
+  left: 380px;
+  font-family: Ubuntu;
+  font-weight: Italic;
+  font-size: 25px;
+  opacity: 1;
+  text-align: left;
+}
+.v264_100 {
+  width: 516px;
+  color: rgba(0, 0, 0, 1);
+  position: absolute;
+  top: 1479px;
+  left: 755px;
+  font-family: Ubuntu;
+  font-weight: Regular;
+  font-size: 25px;
+  opacity: 1;
+  text-align: left;
+}
+.v264_126 {
+  width: 242px;
+  height: 78px;
+  background: rgba(63, 163, 184, 1);
+  opacity: 1;
+  position: absolute;
+  top: 1972px;
+  left: 1133px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+.v264_128 {
+  width: 234px;
+  color: rgba(255, 255, 255, 1);
+  position: absolute;
+  top: 1972px;
+  left: 1216px;
+  font-family: Ubuntu;
+  font-weight: Regular;
+  font-size: 40px;
+  opacity: 1;
+  text-align: left;
+}
+</style>
