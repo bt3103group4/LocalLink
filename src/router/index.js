f5d9f5852c17058d777166d2b838391fce224369
Jialu <dylanshen98@gmail.com>
2021-10-23 20:56:00 +0800

    listings updated

--- src/router/index.js
+++ src/router/index.js
@@ -1,79 +1,84 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 // import Layout from '@/components/Layout.vue'
-import SignUp from '@/views/SignUp.vue'
-import Login from '@/views/Login.vue'
-import Search from '@/views/Search.vue'
-import TouristProfile from '@/views/TouristProfile.vue'
-import EditTouristProfile from '@/views/EditTouristProfile.vue'
-import TourGuideProfile from '@/views/TourGuideProfile.vue'
-import EditTourGuideProfile from '@/views/EditTourGuideProfile.vue'
-import Review from '@/views/Review.vue'
-import Chat from '@/views/Chat.vue'
-import Logout from '@/views/Logout.vue'
-
+import SignUp from "@/views/SignUp.vue";
+import Login from "@/views/Login.vue";
+import Search from "@/views/Search.vue";
+import TouristProfile from "@/views/TouristProfile.vue";
+import EditTouristProfile from "@/views/EditTouristProfile.vue";
+import TourGuideProfile from "@/views/TourGuideProfile.vue";
+import EditTourGuideProfile from "@/views/EditTourGuideProfile.vue";
+import Review from "@/views/Review.vue";
+import Chat from "@/views/Chat.vue";
+import Logout from "@/views/Logout.vue";
+import Listings from "@/views/Listings.vue";
 
 const routes = [
-    {
-    path: '/',
-    name: 'Login',
-    component: Login
-    },
-    {
-    path: '/signup',
-    name: 'SignUp',
-    component: SignUp
-    },
-    {
-    path: '/search',
-    name: 'Search',
-    component: Search
-    },
-    {
-    path: '/touristProfile',
-    name: 'TouristProfile',
-    component: TouristProfile
-    },
-    {
-    path: '/editTouristProfile',
-    name: 'EditTouristProfile',
-    component: EditTouristProfile
-    },
-    {
-    path: '/tourGuideProfile',
-    name: 'TourGuideProfile',
-    component: TourGuideProfile
-    },
-    {
-    path: '/editTourGuideProfile',
-    name: 'EditTourGuideProfile',
-    component: EditTourGuideProfile
-    },
-    {
-    path: '/review',
-    name: 'Review',
-    component: Review
-    },
-    {
-    path: '/chat',
-    name: 'Chat',
-    component: Chat
-    },
-    {
-    path: '/logout',
-    name: 'Logout',
-    component: Logout
-    },    
-    //delete later, for testing
-    // {
-    // path: '/layout',
-    // name: 'Layout',
-    // component: Layout
-    // }
-]
+  {
+    path: "/",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/signup",
+    name: "SignUp",
+    component: SignUp,
+  },
+  {
+    path: "/search",
+    name: "Search",
+    component: Search,
+  },
+  {
+    path: "/touristProfile",
+    name: "TouristProfile",
+    component: TouristProfile,
+  },
+  {
+    path: "/editTouristProfile",
+    name: "EditTouristProfile",
+    component: EditTouristProfile,
+  },
+  {
+    path: "/tourGuideProfile",
+    name: "TourGuideProfile",
+    component: TourGuideProfile,
+  },
+  {
+    path: "/editTourGuideProfile",
+    name: "EditTourGuideProfile",
+    component: EditTourGuideProfile,
+  },
+  {
+    path: "/review",
+    name: "Review",
+    component: Review,
+  },
+  {
+    path: "/chat",
+    name: "Chat",
+    component: Chat,
+  },
+  {
+    path: "/logout",
+    name: "Logout",
+    component: Logout,
+  },
+  {
+    path: "/listings",
+    name: "Listings",
+    component: Listings,
+  },
+
+  //delete later, for testing
+  // {
+  // path: '/layout',
+  // name: 'Layout',
+  // component: Layout
+  // }
+];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
-})
-export default router
-  
+  history: createWebHistory(),
+  routes,
+});
+export default router;
---
+++ src/views/Listings.vue
@@ -0,0 +1,44 @@
+<template>
+  <div style="text-align: center" v-if="user">
+    <NavBar />
+
+    <h3>This is the all listings page.</h3>
+    <ListingTable />
+
+    <LogOut />
+  </div>
+</template>
+
+<script>
+// import firebase from '@/firebase.js'
+import ProfitDisplay from "@/components/ListingTable.vue";
+import NavBar from "@/components/NavBar.vue";
+import LogOut from "@/components/LogOut.vue";
+// import firebase from '@/firebase.js'
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+export default {
+  name: "Listings",
+
+  components: {
+    ListingTable,
+    NavBar,
+    LogOut,
+  },
+
+  data() {
+    return {
+      user: false,
+    };
+  },
+
+  mounted() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.user = user;
+      }
+    });
+  },
+};
+</script>
