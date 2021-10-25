9c595b28e5b46ab841607fad04c0f7fc4307bb22
Rhea Sharma <rheasharmamis@gmail.com>
2021-10-26 02:38:27 +0800

    Add firebase auth to webapp

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
+import firebase from 'firebase'
 
+const firebaseConfig = {
+  apiKey: "AIzaSyB_J3W2iX0B3usVAh8PN9o_-6Z5MB6MhwY",
+  authDomain: "locallink-4d9cd.firebaseapp.com",
+  projectId: "locallink-4d9cd",
+  storageBucket: "locallink-4d9cd.appspot.com",
+  messagingSenderId: "33199670957",
+  appId: "1:33199670957:web:e99731c62c6a364dfd2824"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+export const db = firebaseApp.firestore();
 createApp(App).use(router).mount('#app')
---
+++ src/views/Register.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="register">
+    <div class="centered-container">
+      <h1 class="align-left">
+        Join to unlock the <br />
+        best of <span class="text-blue">Local Link</span>
+      </h1>
+    </div>
+    <form @submit="onFormSubmit" class="centered-container">
+      <div class="register-input-container">
+        <span class="input-desc">First Name</span>
+        <input
+          class="register-text-input"
+          type="text"
+          placeholder="First Name"
+          v-model="user.firstname"
+          required
+        />
+      </div>
+
+      <div class="register-input-container">
+        <span class="input-desc">Last Name</span>
+        <input
+          class="register-text-input"
+          type="text"
+          placeholder="Last Name"
+          v-model="user.lastname"
+          required
+        />
+      </div>
+
+      <div class="register-input-container">
+        <span class="input-desc">User Name</span>
+        <input
+          class="register-text-input"
+          type="text"
+          placeholder="Enter a unique user name"
+          v-model="user.username"
+          required
+        />
+      </div>
+
+      <div class="register-input-container">
+        <span class="input-desc">Email</span>
+        <input
+          class="register-text-input"
+          type="text"
+          placeholder="Enter your email address"
+          v-model="user.email"
+          required
+        />
+      </div>
+
+      <div class="register-input-container">
+        <span class="input-desc">Password</span>
+        <input
+          class="register-text-input"
+          type="password"
+          placeholder="Enter your password"
+          v-model="user.password"
+          required
+        />
+        <br />
+      </div>
+
+      <input class="register-submit" type="submit" value="Join" />
+      <p>
+        Already have an account? <router-link to="/">Login instead</router-link>
+      </p>
+    </form>
+  </div>
+</template>
+
+<style>
+/* input[type=text], input[type=password] */
+
+.register-text-input {
+  width: 100%;
+  padding: 12px 20px;
+  margin-bottom: 8px;
+  box-sizing: border-box;
+  border-radius: 10px;
+}
+
+.register-submit {
+  width: 50%;
+  padding: 12px 20px;
+  margin-bottom: 8px;
+  box-sizing: border-box;
+  border-radius: 50px;
+  color: white;
+  background-color: black;
+}
+
+.register {
+  width: 100vw;
+}
+
+@media only screen and (min-width: 728px) {
+  .register {
+    width: 30vw;
+    border: 2px solid black;
+    border-radius: 40px;
+  }
+
+  .register-input-container {
+    width: 60% !important;
+  }
+}
+
+.text-blue {
+  color: #40a3b9;
+}
+
+.align-left {
+  text-align: left;
+}
+
+.centered-container {
+  display: flex;
+  flex-direction: column;
+  /* justify-content:flex-start; */
+  align-items: center;
+}
+
+.register-input-container {
+  width: 80%;
+  margin-top: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+</style>
