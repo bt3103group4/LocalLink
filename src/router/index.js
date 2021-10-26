6806d97d33c478850aed1a9de6760738679898f2
Jialu <dylanshen98@gmail.com>
2021-10-26 15:13:44 +0800

    Add comment

--- src/router/index.js
+++ src/router/index.js
@@ -1,73 +1,73 @@
 import { createRouter, createWebHistory } from "vue-router";
 // import Layout from '@/components/Layout.vue'
-import SignUp from '@/views/SignUp.vue'
-import Login from '@/views/Login.vue'
-import Search from '@/views/Search.vue'
-import TouristProfile from '@/views/TouristProfile.vue'
-import TourGuideProfile from '@/views/TourGuideProfile.vue'
-import EditTourGuideProfile from '@/views/EditTourGuideProfile.vue'
-import Review from '@/views/Review.vue'
-import Chat from '@/views/Chat.vue'
-import Logout from '@/views/Logout.vue'
-import Register from '@/views/Register.vue'
-
+import SignUp from "@/views/SignUp.vue";
+import Login from "@/views/Login.vue";
+import Search from "@/views/Search.vue";
+import TouristProfile from "@/views/TouristProfile.vue";
+import TourGuideProfile from "@/views/TourGuideProfile.vue";
+import EditTourGuideProfile from "@/views/EditTourGuideProfile.vue";
+import Review from "@/views/Review.vue";
+import Chat from "@/views/Chat.vue";
+import Logout from "@/views/Logout.vue";
+import Register from "@/views/Register.vue";
+//test
 const routes = [
-    {
-    path: '/',
-    name: 'Login',
-    component: Login
-    },
-    {
-    path: '/register',
-    name: 'Register',
-    component: Register
-    },
-    {
-    path: '/signup',
-    name: 'SignUp',
-    component: SignUp
-    },
-    {
-    path: '/search',
-    name: 'Search',
-    component: Search
-    },
-    {
-    path: '/touristProfile',
-    name: 'TouristProfile',
-    component: TouristProfile
-    },
-    {
-    path: '/tourGuideProfile',
-    name: 'TourGuideProfile',
-    component: TourGuideProfile
-    },
-    {
-    path: '/editTourGuideProfile',
-    name: 'EditTourGuideProfile',
-    component: EditTourGuideProfile
-    },
-    {
-    path: '/review',
-    name: 'Review',
-    component: Review
-    },
-    {
-    path: '/chat',
-    name: 'Chat',
-    component: Chat
-    },
-    {
-    path: '/logout',
-    name: 'Logout',
-    component: Logout
-    },    
-    //delete later, for testing
-    // {
-    // path: '/layout',
-    // name: 'Layout',
-    // component: Layout
-    // }
+  {
+    path: "/",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+  },
+  {
+    path: "/signup",
+    name: "SignUp",
+    component: SignUp,
+  },
+  {
+    path: "/search",
+    name: "Search",
+    component: Search,
+  },
+  {
+    path: "/touristProfile",
+    name: "TouristProfile",
+    component: TouristProfile,
+  },
+  {
+    path: "/tourGuideProfile",
+    name: "TourGuideProfile",
+    component: TourGuideProfile,
+  },
+  {
+    path: "/editTourGuideProfile",
+    name: "EditTourGuideProfile",
+    component: EditTourGuideProfile,
+  },
+  {
+    path: "/review",
+    name: "Review",
+    component: Review,
+  },
+  {
+    path: "/chat",
+    name: "Chat",
+    component: Chat,
+  },
+  {
+    path: "/logout",
+    name: "Logout",
+    component: Logout,
+  },
+  //delete later, for testing
+  // {
+  // path: '/layout',
+  // name: 'Layout',
+  // component: Layout
+  // }
 ];
 
 const router = createRouter({
