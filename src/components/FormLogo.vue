e755cf6c1ee85c42c4b9cb65ca93141b0b455702
Rhea Sharma <rheasharmamis@gmail.com>
2021-10-31 11:27:54 +0800

    Create logo component for forms

---
+++ src/components/FormLogo.vue
@@ -0,0 +1,29 @@
+<template>
+    <span class="form-logo"></span>
+</template>
+
+<script>
+export default {
+    name: "FormLogo"
+}
+</script>
+
+<style scoped>
+.form-logo {
+  width: 80px;
+  height: 80px;
+  background: url("~@/assets/logo.png");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  opacity: 1;
+  /* position: absolute; */
+  /* top: 22px; */
+  /* left: 28px; */
+  border-top-left-radius: 50px;
+  border-top-right-radius: 50px;
+  border-bottom-left-radius: 50px;
+  border-bottom-right-radius: 50px;
+  overflow: hidden;
+}
+</style>
