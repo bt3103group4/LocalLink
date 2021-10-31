062e59fb775918bf59db1f214dfe493a80224dc2
Rhea Sharma <rheasharmamis@gmail.com>
2021-10-31 11:25:12 +0800

    Update landing page

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -3,10 +3,47 @@
 
 <div class="landing-page">
     <div class="centered-container">
-      <h1>Landing Page</h1>
+      <h1>Local Link</h1>
+      <span class = landing-logo></span>
     </div>
-    <button><router-link to="/logintourist">Tourist</router-link></button>
-    <button><router-link to="/logintourguide">TourGuide</router-link></button>
-    
+    <div class ="two-col">
+    <button class="landing-button"><router-link to="/logintourist">Tourist</router-link></button>
+    <button class="landing-button"><router-link to="/logintourguide">TourGuide</router-link></button>
+    </div>
 </div>
 </template>
+
+<style scoped>
+.landing-logo{
+  background: url("~@/assets/logo.png");
+}
+
+.two-col{
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding-right: 10px;
+
+}
+
+.landing-button{
+  background-color: rgb(136, 195, 218); /* Green */
+  border: none;
+  color: black;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  border-radius: 5px;
+  font-size: 16px;
+}
+
+</style>
+
+
+
+
+
+
+
