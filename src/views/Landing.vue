4b267cf709981960107c1e737a0c10be899dd9e5
Rhea Sharma <rheasharmamis@gmail.com>
2021-10-31 12:11:12 +0800

    Improved Landing Page

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -3,19 +3,39 @@
 
 <div class="landing-page">
     <div class="centered-container">
-      <h1>Local Link</h1>
+      <h1>Explore Local Link as a</h1>
       <span class = landing-logo></span>
     </div>
     <div class ="two-col">
     <button class="landing-button"><router-link to="/logintourist">Tourist</router-link></button>
-    <button class="landing-button"><router-link to="/logintourguide">TourGuide</router-link></button>
+    <button class="landing-button"><router-link to="/logintourguide" >TourGuide</router-link></button>
+    </div>
+    <div class="logo">
+    <FormLogo/>
     </div>
 </div>
 </template>
 
+<script>
+import FormLogo from '@/components/FormLogo.vue'
+export default {
+    components: { FormLogo },
+}
+</script>
+
+
 <style scoped>
-.landing-logo{
-  background: url("~@/assets/logo.png");
+.logo{
+  display: flex;
+}
+
+/* body {
+  background-color: rgb(153, 214, 223);
+} */
+.centered-container{
+display: flex;
+align-items: center;
+justify-content: center;
 }
 
 .two-col{
@@ -27,19 +47,51 @@
 
 }
 
-.landing-button{
-  background-color: rgb(136, 195, 218); /* Green */
-  border: none;
-  color: black;
-  padding: 15px 32px;
+.landing-button {
+  appearance: none;
+  background-color: #7394d3;
+  opacity: 0.9;
+  border: 2px solid white;
+  border-radius: 15px;
+  box-sizing: border-box;
+  color: #FFFFFF;
+  cursor: pointer;
+  display: inline-block;
+  font-family: Roobert,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
+  font-size: 16px;
+  font-weight: 600;
+  line-height: normal;
+  margin: 0;
+  min-height: 60px;
+  min-width: 0;
+  outline: none;
+  padding: 16px 24px;
   text-align: center;
   text-decoration: none;
-  display: inline-block;
-  border-radius: 5px;
-  font-size: 16px;
+  transition: all 300ms cubic-bezier(.23, 1, 0.32, 1);
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  width: 100%;
+  will-change: transform;
+}
+
+.landing-button:disabled {
+  pointer-events: none;
+}
+
+.landing-button:hover {
+  box-shadow: rgba(0, 0, 0, 0.25) 0 8px 15px;
+  transform: translateY(-2px);
+}
+
+.landing-button:active {
+  box-shadow: none;
+  transform: translateY(0);
 }
 
 </style>
+
 
 
 
