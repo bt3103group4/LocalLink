abec3d46c20b96fec7e25d501b8d207f31d66574
Rhea Sharma <rheasharmamis@gmail.com>
2021-10-31 11:45:34 +0800

    Style tourguide registration form

--- src/views/RegisterTourGuide.vue
+++ src/views/RegisterTourGuide.vue
@@ -74,3 +74,74 @@
   
 </template>
 
+<style scoped>
+/* input[type=text], input[type=password] */
+
+.logo-and-title {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+
+.logo-and-title > * {
+  margin: 8px;
+}
+
+.register-text-input {
+  width: 100%;
+  padding: 12px 20px;
+  margin-bottom: 8px;
+  box-sizing: border-box;
+  border-radius: 10px;
+}
+
+.register-submit {
+  width: 50%;
+  padding: 12px 20px;
+  margin-bottom: 8px;
+  box-sizing: border-box;
+  border-radius: 50px;
+  color: white;
+  background-color: black;
+}
+
+.register {
+  width: 100vw;
+}
+
+@media only screen and (min-width: 728px) {
+  .register {
+    width: 40vw;
+    border: 2px solid black;
+    border-radius: 40px;
+  }
+
+  .register-input-container {
+    width: 60% !important;
+  }
+}
+
+.text-blue {
+  color: #40a3b9;
+}
+
+.align-left {
+  text-align: left;
+}
+
+.centered-container {
+  display: flex;
+  flex-direction: column;
+  /* justify-content:flex-start; */
+  align-items: center;
+}
+
+.register-input-container {
+  width: 80%;
+  margin-top: 5px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+</style>
