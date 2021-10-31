313ee05441733cfdd495fe7314ae38af6a1cd41e
Rhea Sharma <rheasharmamis@gmail.com>
2021-10-31 11:41:18 +0800

    Add logo to login tourguide page

--- src/views/LoginTourGuide.vue
+++ src/views/LoginTourGuide.vue
@@ -1,10 +1,19 @@
 <template>
-<div class="landing-page">
+<div class="login">
+  <div class="logo-and-title">
+      <FormLogo/>
     <div class="centered-container">
-      <h1>Landing Page</h1>
+        <h1>Welcome Back Tour Guides!</h1>
     </div>
-    <button>Login<router-link to="/tourguideprofile">TourGuide</router-link></button>
-    <button>Register<router-link to="/registertourguide">TourGuide</router-link></button>
-    
+    </div>
 </div>
 </template>
+import FormLogo from '@/components/FormLogo.vue'
+    components: { FormLogo },
+.logo-and-title {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+}
+
