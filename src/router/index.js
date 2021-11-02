aaa656bec4b043d638a5bb1153e8705adf874fef
Jialu <dylanshen98@gmail.com>
2021-11-02 11:23:49 +0800

    deleted listings.vue

--- src/router/index.js
+++ src/router/index.js
@@ -6,134 +6,127 @@ import TourGuideProfile from "@/views/TourGuideProfile.vue";
 import EditTourGuideProfile from "@/views/EditTourGuideProfile.vue";
 import Chat from "@/views/Chat.vue";
 import Logout from "@/views/Logout.vue";
-import Listings from "@/views/Listings.vue";
 import ListingsNature from "@/views/ListingsNature.vue";
 import ListingsAdv from "@/views/ListingsAdv.vue";
 import ListingsBeach from "@/views/ListingsBeach.vue";
 import EditUserProfile from "@/views/EditUserProfile.vue";
 // import SignUp from '@/views/SignUp.vue'
-import RegisterTourist from '@/views/RegisterTourist.vue'
-import LoginTourist from '@/views/LoginTourist.vue'
-import RegisterTourGuide from '@/views/RegisterTourGuide.vue'
-import LoginTourGuide from '@/views/LoginTourGuide.vue'
-import Landing from '@/views/Landing.vue'
-import EditTour from '@/views/EditTour.vue'
-import NewReviewTourGuide from '@/views/NewReviewTourGuide.vue'
-import NewReviewTourist from '@/views/NewReviewTourGuide.vue'
+import RegisterTourist from "@/views/RegisterTourist.vue";
+import LoginTourist from "@/views/LoginTourist.vue";
+import RegisterTourGuide from "@/views/RegisterTourGuide.vue";
+import LoginTourGuide from "@/views/LoginTourGuide.vue";
+import Landing from "@/views/Landing.vue";
+import EditTour from "@/views/EditTour.vue";
+import NewReviewTourGuide from "@/views/NewReviewTourGuide.vue";
+import NewReviewTourist from "@/views/NewReviewTourGuide.vue";
 
 const routes = [
-    {
-    path: '/',
-    name: 'Landing',
-    component: Landing
-    },
-    {
-    path: '/logintourguide',
-    name: 'LoginTourGuide',
-    component: LoginTourGuide
-    },
-    {
-    path: '/registertourguide',
-    name: 'RegisterTourGuide',
-    component: RegisterTourGuide
-    },
-    {
-    path: '/logintourist',
-    name: 'LoginTourist',
-    component: LoginTourist
-    },
-    {
-    path: '/registertourist',
-    name: 'RegisterTourist',
-    component: RegisterTourist
-    },
-    // {
-    // path: '/signup',
-    // name: 'SignUp',
-    // component: SignUp
-    // },
-    {
-    path: '/search',
-    name: 'Search',
-    component: Search
-    },
-    {
+  {
+    path: "/",
+    name: "Landing",
+    component: Landing,
+  },
+  {
+    path: "/logintourguide",
+    name: "LoginTourGuide",
+    component: LoginTourGuide,
+  },
+  {
+    path: "/registertourguide",
+    name: "RegisterTourGuide",
+    component: RegisterTourGuide,
+  },
+  {
+    path: "/logintourist",
+    name: "LoginTourist",
+    component: LoginTourist,
+  },
+  {
+    path: "/registertourist",
+    name: "RegisterTourist",
+    component: RegisterTourist,
+  },
+  // {
+  // path: '/signup',
+  // name: 'SignUp',
+  // component: SignUp
+  // },
+  {
+    path: "/search",
+    name: "Search",
+    component: Search,
+  },
+  {
     path: "/newtour",
     name: "NewTour",
     component: NewTour,
-    },
-    {
-      path: "/edittour",
-      name: "EditTour",
-      component: EditTour,
-    },
-    {
-    path: '/touristProfile',
-    name: 'TouristProfile',
-    component: TouristProfile
-    },
-    {
-    path: '/editUserProfile',
-    name: 'EditUserProfile',
-    component: EditUserProfile
-    },
-    {
-    path: '/tourGuideProfile',
-    name: 'TourGuideProfile',
-    component: TourGuideProfile
-    },
-    {
-    path: '/editTourGuideProfile',
-    name: 'EditTourGuideProfile',
-    component: EditTourGuideProfile
-    },
-    {
-    path: '/chat',
-    name: 'Chat',
-    component: Chat
-    },
-    {
-    path: '/logout',
-    name: 'Logout',
-    component: Logout
-    },    
-
-    {
-    path: "/listings",
-    name: "Listings",
-    component: Listings,
-    },
-    {
-      path: "/listingsnature",
-      name: "ListingsNature",
-      component: ListingsNature,
-    },
-    {
-      path: "/listingsadv",
-      name: "ListingsAdv",
-      component: ListingsAdv,
-    },
-    {
-      path: "/listingsbeach",
-      name: "ListingsBeach",
-      component: ListingsBeach,
-    },
-    {
-      path: "/newreviewtourguide",
-      name: "NewReviewTourGuide",
-      component: NewReviewTourGuide,
-    },
-    {
-      path: "/newreviewtourist",
-      name: "NewReviewTourist",
-      component: NewReviewTourist,
-    },
-    //delete later, for testing
-    // {
-    // path: '/layout',
-    // name: 'Layout',
-    // component: Layout
-    // }
+  },
+  {
+    path: "/edittour",
+    name: "EditTour",
+    component: EditTour,
+  },
+  {
+    path: "/touristProfile",
+    name: "TouristProfile",
+    component: TouristProfile,
+  },
+  {
+    path: "/editUserProfile",
+    name: "EditUserProfile",
+    component: EditUserProfile,
+  },
+  {
+    path: "/tourGuideProfile",
+    name: "TourGuideProfile",
+    component: TourGuideProfile,
+  },
+  {
+    path: "/editTourGuideProfile",
+    name: "EditTourGuideProfile",
+    component: EditTourGuideProfile,
+  },
+  {
+    path: "/chat",
+    name: "Chat",
+    component: Chat,
+  },
+  {
+    path: "/logout",
+    name: "Logout",
+    component: Logout,
+  },
+  {
+    path: "/listingsnature",
+    name: "ListingsNature",
+    component: ListingsNature,
+  },
+  {
+    path: "/listingsadv",
+    name: "ListingsAdv",
+    component: ListingsAdv,
+  },
+  {
+    path: "/listingsbeach",
+    name: "ListingsBeach",
+    component: ListingsBeach,
+  },
+  {
+    path: "/newreviewtourguide",
+    name: "NewReviewTourGuide",
+    component: NewReviewTourGuide,
+  },
+  {
+    path: "/newreviewtourist",
+    name: "NewReviewTourist",
+    component: NewReviewTourist,
+  },
+  //delete later, for testing
+  // {
+  // path: '/layout',
+  // name: 'Layout',
+  // component: Layout
+  // }
 ];
 
 const router = createRouter({
