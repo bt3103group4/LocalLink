0e466185b5f157f2f58384deadebf7847bdfce31
Rhea Sharma <rheasharmamis@gmail.com>
2021-11-06 13:58:28 +0800

    Define basic landing styling constants

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -17,6 +17,38 @@ export default {
 
 
 <style scoped>
+
+:root {
+  --landing-color-gray-500: #595959;
+  --landing-color-gray-700: #999999;
+  --landing-color-gray-900: #d9d9d9;
+  --landing-color-danger-300: #a22020;
+  --landing-color-danger-500: #bf2626;
+  --landing-color-danger-700: #e14747;
+  --landing-color-gray-black: #000000;
+  --landing-color-gray-white: #ffffff;
+  --landing-space-space-unit: 8px;
+  --landing-color-primary-100: #3f68b5ff;
+  --landing-color-primary-300: #0074f0;
+  --landing-color-primary-500: #14a9ff;
+  --landing-color-primary-700: #85dcff;
+  --landing-color-success-300: #199033;
+  --landing-color-success-500: #32a94c;
+  --landing-color-success-700: #4cc366;
+  --landing-radius-radius-round: 50%;
+  --landing-size-size-maxcontent: 1414px;
+  --landing-space-space-halfunit: 4px;
+  --landing-space-space-tenunits: 80px;
+  --landing-radius-radius-radius2: 2px;
+  --landing-radius-radius-radius4: 4px;
+  --landing-radius-radius-radius8: 8px;
+  --landing-space-space-fiveunits: 40px;
+  --landing-space-space-fourunits: 32px;
+  --landing-radius-radius-radius24: 24px;
+  --landing-space-space-doubleunit: 16px;
+  --landing-space-space-tripleunit: 24px;
+}
+
 .landing-container {
   width: 100%;
   height: auto;
