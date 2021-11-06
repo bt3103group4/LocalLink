c68c28eec9b2c37d277e8aba180d0a22b183c56f
Rhea Sharma <rheasharmamis@gmail.com>
2021-11-06 19:47:25 +0800

    Add contact form on landing page

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -200,6 +200,48 @@
         </div>
       </div>
     </div>
+    <div class="landing-section-separator"></div>
+    <div class="landing-get-in-touch">
+      <h2 class="section-heading landing-features-text">Get in touch</h2>
+      <div class="landing-content-container6">
+        <div class="landing-form-container">
+          <span class="big-card-heading landing-heading">Send us a message</span>
+          <input type="text" required="true" placeholder="Name" class="landing-text-input landing-name5" />
+          <input type="text" required="true" placeholder="E-mail" class="landing-text-input landing-email" />
+          <textarea placeholder="Your Message" class="landing-text-area landing-message"></textarea>
+          <button class="anchor landing-button landing-cta-btn2">SEND</button>
+        </div>
+        <div class="landing-locations-container">
+          <div class="landing-location">
+            <span class="landing-address-heading">NUS, Singapore</span>
+            <div class="landing-address">
+              <svg viewBox="0 0 1024 1024" class="landing-address-icon">
+                <path
+                  d="M512 0c-176.732 0-320 143.268-320 320 0 320 320 704 320 704s320-384 320-704c0-176.732-143.27-320-320-320zM512 512c-106.040 0-192-85.96-192-192s85.96-192 192-192 192 85.96 192 192-85.96 192-192 192z"
+                ></path>
+              </svg>
+              <span class="section-text">13, Computing Drive, COM1, 117417</span>
+            </div>
+            <div class="landing-address">
+              <svg viewBox="0 0 1024 1024" class="landing-address-icon">
+                <path
+                  d="M854 342v-86l-342 214-342-214v86l342 212zM854 170q34 0 59 26t25 60v512q0 34-25 60t-59 26h-684q-34 0-59-26t-25-60v-512q0-34 25-60t59-26h684z"
+                ></path>
+              </svg>
+              <span class="section-text">rheasharma.nus@gmail.com</span>
+            </div>
+            <div class="landing-address">
+              <svg viewBox="0 0 804.5714285714286 1024" class="landing-address-icon">
+                <path
+                  d="M804.571 708.571c0 20.571-9.143 60.571-17.714 79.429-12 28-44 46.286-69.714 60.571-33.714 18.286-68 29.143-106.286 29.143-53.143 0-101.143-21.714-149.714-39.429-34.857-12.571-68.571-28-100-47.429-97.143-60-214.286-177.143-274.286-274.286-19.429-31.429-34.857-65.143-47.429-100-17.714-48.571-39.429-96.571-39.429-149.714 0-38.286 10.857-72.571 29.143-106.286 14.286-25.714 32.571-57.714 60.571-69.714 18.857-8.571 58.857-17.714 79.429-17.714 4 0 8 0 12 1.714 12 4 24.571 32 30.286 43.429 18.286 32.571 36 65.714 54.857 97.714 9.143 14.857 26.286 33.143 26.286 50.857 0 34.857-103.429 85.714-103.429 116.571 0 15.429 14.286 35.429 22.286 49.143 57.714 104 129.714 176 233.714 233.714 13.714 8 33.714 22.286 49.143 22.286 30.857 0 81.714-103.429 116.571-103.429 17.714 0 36 17.143 50.857 26.286 32 18.857 65.143 36.571 97.714 54.857 11.429 5.714 39.429 18.286 43.429 30.286 1.714 4 1.714 8 1.714 12z"
+                ></path>
+              </svg>
+              <span class="section-text">6516 2727</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <div id="team" class="landing-section-separator"></div>
     <div class="landing-footer-container">
       <div class="landing-footer">
