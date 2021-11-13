4b7790cb62b35bbf39fe9d65b3e52718fb0f3980
Rhea Sharma <rheasharmamis@gmail.com>
2021-11-14 04:32:48 +0800

    Create basic Virtual Tour Page

---
+++ src/views/VirtualTour.vue
@@ -0,0 +1,16 @@
+  </body>
+</template>
+
+<script>
+import SettingsButton from "@/components/SettingsButton.vue";
+import NavBar from "@/components/NavBar.vue";
+import Logo from "@/components/Logo.vue";
+
+export default {
+  name: "VirtualTour",
+  data() {},
+  components: { SettingsButton, NavBar, Logo },
+  methods: {},
+};
+</script>
+
