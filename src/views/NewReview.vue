e8775b7af742890f8ac2093df11e57787f6b560d
limzihui <limzihui2302@gmail.com>
2021-11-14 01:05:26 +0800

    made tourreviews responsive

--- src/views/NewReview.vue
+++ src/views/NewReview.vue
@@ -75,7 +75,7 @@ body{
   color: rgba(0, 0, 0, 1);
   position: absolute;
   top: 140px;
-  left: 388px;
+  left:25%;
   text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   font-family: Ubuntu;
   font-weight: Bold;
